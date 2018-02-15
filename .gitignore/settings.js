b8340953e40eed62601fb1c799cb535c4c79e5ef
code-in-time <andrew.crawford.za@gmail.com>
2018-02-15 09:16:05 +0200

    First commit to add settings

---
+++ .gitignore/settings.js
@@ -0,0 +1,14 @@
+{
+    "editor.fontSize": 11,
+    // If using windows
+    "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe",
+    "files.autoSave": "off",
+    // Show all mu files
+    "files.exclude": {
+        "**/.git": false,
+        "**/.svn": false,
+        "**/.hg": false,
+        "**/CVS": false,
+        "**/.DS_Store": false
+      }
+}
