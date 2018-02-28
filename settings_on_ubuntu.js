ff890ec9333a2121f8d9be754e214a1e47d1d043
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-02-28 19:25:41 +0200

    Create settings_on_ubuntu.js

---
+++ settings_on_ubuntu.js
@@ -0,0 +1,21 @@
+{
+  // Controls the font size in pixels of the terminal.
+  "terminal.integrated.fontSize": 11,
+  // Controls the font size in pixels of the terminal.
+  "editor.fontSize": 13,
+  "window.zoomLevel": -1,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+  },
+  "files.exclude": {
+    "**/.git": false,
+    "**/.svn": false,
+    "**/.hg": false,
+    "**/.DS_Store": false
+  },
+    // Always show the ESlint status bar item.
+    "eslint.alwaysShowStatus": true,
+    "prettier.singleQuote": true,
+    "eslint.enable": false,
+    "files.autoSave": "off",
+}
